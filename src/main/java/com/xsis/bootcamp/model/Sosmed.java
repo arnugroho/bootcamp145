8a2b81d5f692416c191be2054bb82c2dad7697a5
BYO\Hari <harircmd@gmail.com>
2018-04-23 14:32:00 +0700

    Revert "Revert "hapus lokal""
    This reverts commit 0f270a4374ff95a125066d8f99c507c72d838d2e.

---
+++ src/main/java/com/xsis/bootcamp/model/Sosmed.java
@@ -0,0 +1,19 @@
+package com.xsis.bootcamp.model;
+
+public class Sosmed {
+	private int idSosmed;
+	private String namaSosmed;
+	public int getIdSosmed() {
+		return idSosmed;
+	}
+	public void setIdSosmed(int idSosmed) {
+		this.idSosmed = idSosmed;
+	}
+	public String getNamaSosmed() {
+		return namaSosmed;
+	}
+	public void setNamaSosmed(String namaSosmed) {
+		this.namaSosmed = namaSosmed;
+	}
+	
+}
