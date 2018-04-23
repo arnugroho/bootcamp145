6e2090664b39d318c70bc00e5340cc447099fe44
Dams <heydamas@gmail.com>
2018-04-23 17:19:47 +0700

    Mahasiswa Dao Impl Nich..

---
+++ src/main/java/com/xsis/bootcamp/dao/impl/MahasiswaDaoImpl.java
@@ -0,0 +1,63 @@
+package com.xsis.bootcamp.dao.impl;
+
+import java.util.Collection;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+import com.xsis.bootcamp.dao.MahasiswaDao;
+import com.xsis.bootcamp.model.Mahasiswa;
+
+public class MahasiswaDaoImpl extends HibernateDaoSupport implements MahasiswaDao {
+	@Autowired
+	public MahasiswaDaoImpl(SessionFactory sessionFactory) {
+		setSessionFactory(sessionFactory);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<Mahasiswa> listMahasiswa() throws Exception {
+		Session session = this.getSessionFactory().getCurrentSession();
+		String hql = "from mahasiswa";
+		Query query = session.createQuery(hql);
+		return query.list();
+	}
+
+	@Override
+	public Mahasiswa getMahasiswa(int nim) throws Exception {
+		Session session = this.getSessionFactory().getCurrentSession();
+		String hql = "from mahasiswa where nim = :nim";
+		Query query = session.createQuery(hql);
+		query.setInteger("nim", nim);
+		if (query.list().size() > 0) {
+			return (Mahasiswa) query.list().get(0);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void insertMahasiswa(Mahasiswa mahasiswa) throws Exception {
+		Session session = this.getSessionFactory().getCurrentSession();
+		session.save(mahasiswa);
+
+	}
+
+	@Override
+	public void updateMahasiswa(Mahasiswa mahasiswa) throws Exception {
+		Session session = this.getSessionFactory().getCurrentSession();
+		session.update(mahasiswa);
+
+	}
+
+	@Override
+	public void deleteMahasiswa(Mahasiswa mahasiswa) throws Exception {
+		Session session = this.getSessionFactory().getCurrentSession();
+		session.delete(mahasiswa);
+
+	}
+
+}
