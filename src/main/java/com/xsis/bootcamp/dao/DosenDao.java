526d700ab101b16f3de5435c86ddbf2ced684689
Haswanda Imawan <haswandaimawan@gmail.com>
2018-04-23 16:39:59 +0700

    tambah getDosen di Dao.DosenDao & Buat DosenServiceImpl gan hehe :)

--- src/main/java/com/xsis/bootcamp/dao/DosenDao.java
+++ src/main/java/com/xsis/bootcamp/dao/DosenDao.java
@@ -7,6 +7,7 @@ import com.xsis.bootcamp.model.Dosen;
 public interface DosenDao {
 	
 	public Collection<Dosen> listDosen() throws Exception;
+	public Dosen getDosen (int nikDosen) throws Exception;
 	public void insertDosen(Dosen dosen) throws Exception;
 	public void updateDosen(Dosen dosen) throws Exception;
 	public void deleteDosen(Dosen dosen) throws Exception;
