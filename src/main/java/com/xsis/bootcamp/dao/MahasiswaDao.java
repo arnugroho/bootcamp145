0f688c9cb95feb066ef3b2fd6760f30bda55f85d
Dams <heydamas@gmail.com>
2018-04-23 16:50:33 +0700

    Interface Mahasiswa Dao Nich..

---
+++ src/main/java/com/xsis/bootcamp/dao/MahasiswaDao.java
@@ -0,0 +1,13 @@
+package com.xsis.bootcamp.dao;
+
+import java.util.Collection;
+
+import com.xsis.bootcamp.model.Mahasiswa;
+
+public interface MahasiswaDao {
+	public Collection<Mahasiswa> listMahasiswa() throws Exception;
+	public void insertMahasiswa (Mahasiswa mahasiswa) throws Exception;
+	public void updateMahasiswa (Mahasiswa mahasiswa) throws Exception;
+	public void deleteMahasiswa (Mahasiswa mahasiswa) throws Exception;
+
+}
