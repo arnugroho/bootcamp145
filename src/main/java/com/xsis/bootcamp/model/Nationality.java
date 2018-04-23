73776e499f17ed28728c2c3ef7c139345465a59b
Andreas-PC\Andreas <andreas.yoga5757@gmail.com>
2018-04-23 11:55:27 +0700

    Penambahan sysout di nasionality

--- src/main/java/com/xsis/bootcamp/model/Nationality.java
+++ src/main/java/com/xsis/bootcamp/model/Nationality.java
@@ -3,6 +3,9 @@ package com.xsis.bootcamp.model;
 public class Nationality {
  public String hargadri() {
 	 String n ="ini adalah method kayaknya";
+	System.out.println(n);
 	 return null;
  }
+
+
 }
