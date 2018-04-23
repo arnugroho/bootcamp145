e983f4bc88560b6c5cd7fd40137a378b2c3e3432
Rezky Pratomo <rezkypratomo7@gmail.com>
2018-04-23 11:58:05 +0700

    Penambahan getter setter model religion

--- src/main/java/com/xsis/bootcamp/model/Religion.java
+++ src/main/java/com/xsis/bootcamp/model/Religion.java
@@ -1,5 +1,23 @@
 package com.xsis.bootcamp.model;
 
 public class Religion {
+	private int idReligion;
+	private String namaReligion;
+
+	public int getIdReligion() {
+		return idReligion;
+	}
+
+	public void setIdReligion(int idReligion) {
+		this.idReligion = idReligion;
+	}
+
+	public String getNamaReligion() {
+		return namaReligion;
+	}
+
+	public void setNamaReligion(String namaReligion) {
+		this.namaReligion = namaReligion;
+	}
 
 }
