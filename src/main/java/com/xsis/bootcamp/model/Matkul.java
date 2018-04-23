5d0d1c689217d43498ea6519162801dac1e03206
BYO\Hari <harircmd@gmail.com>
2018-04-23 15:21:49 +0700

    Model matkul

---
+++ src/main/java/com/xsis/bootcamp/model/Matkul.java
@@ -0,0 +1,34 @@
+package com.xsis.bootcamp.model;
+
+public class Matkul {
+	private int id_matkul;
+	private String nama;
+	private int sks;
+	private int id_jurusan;
+	public int getId_matkul() {
+		return id_matkul;
+	}
+	public void setId_matkul(int id_matkul) {
+		this.id_matkul = id_matkul;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getSks() {
+		return sks;
+	}
+	public void setSks(int sks) {
+		this.sks = sks;
+	}
+	public int getId_jurusan() {
+		return id_jurusan;
+	}
+	public void setId_jurusan(int id_jurusan) {
+		this.id_jurusan = id_jurusan;
+	}
+	
+
+}
