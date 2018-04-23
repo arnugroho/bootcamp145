b144ce018130194f92f994e60689baf7755f4c18
BYO\Hari <harircmd@gmail.com>
2018-04-23 17:00:35 +0700

    Revisi model Matkul

--- src/main/java/com/xsis/bootcamp/model/Matkul.java
+++ src/main/java/com/xsis/bootcamp/model/Matkul.java
@@ -1,33 +1,56 @@
 package com.xsis.bootcamp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "matkul")
 public class Matkul {
-	private int id_matkul;
+	private int idMatkul;
 	private String nama;
 	private int sks;
-	private int id_jurusan;
-	public int getId_matkul() {
-		return id_matkul;
+	private Jurusan jurusanDesc;
+
+	@GeneratedValue
+	@Id
+	@Column(name = "idmatkul", length = 11, nullable = false)
+	public int getIdMatkul() {
+		return idMatkul;
 	}
-	public void setId_matkul(int id_matkul) {
-		this.id_matkul = id_matkul;
+
+	public void setIdMatkul(int idMatkul) {
+		this.idMatkul = idMatkul;
 	}
+
+	@Column(name = "nama", length = 45, nullable = false)
 	public String getNama() {
 		return nama;
 	}
+
 	public void setNama(String nama) {
 		this.nama = nama;
 	}
+
+	@Column(name = "sks", length = 2, nullable = false)
 	public int getSks() {
 		return sks;
 	}
+
 	public void setSks(int sks) {
 		this.sks = sks;
 	}
-	public int getId_jurusan() {
-		return id_jurusan;
+
+	@ManyToOne
+	@JoinColumn(name = "jurusan", updatable = false, nullable = false, insertable = false)
+	public Jurusan getJurusanDesc() {
+		return jurusanDesc;
 	}
-	public void setId_jurusan(int id_jurusan) {
-		this.id_jurusan = id_jurusan;
+
+	public void setJurusanDesc(Jurusan jurusanDesc) {
+		this.jurusanDesc = jurusanDesc;
 	}
 	
 
