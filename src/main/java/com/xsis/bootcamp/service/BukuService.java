d82f8955e7765bfe78c47db6b9483a3a2e7ba7f5
akbar riyan nugroho - arnugroho <nugroho.akbar.r@gmail.com>
2018-04-27 07:02:44 +0700

    ketiga : buat service dan implnya

---
+++ src/main/java/com/xsis/bootcamp/service/BukuService.java
@@ -0,0 +1,17 @@
+package com.xsis.bootcamp.service;
+
+import java.util.Collection;
+
+import com.xsis.bootcamp.model.Buku;
+
+public interface BukuService {
+	
+	public void insert(Buku buku) throws Exception;
+
+	public void update(Buku buku) throws Exception;
+
+	public void delete(Buku buku) throws Exception;
+	
+	public Collection<Buku> listAll() throws Exception;
+
+}
---
+++ src/main/java/com/xsis/bootcamp/service/impl/BukuServiceImpl.java
@@ -0,0 +1,42 @@
+package com.xsis.bootcamp.service.impl;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.xsis.bootcamp.dao.BukuDao;
+import com.xsis.bootcamp.model.Buku;
+import com.xsis.bootcamp.service.BukuService;
+
+@Transactional
+@Service
+public class BukuServiceImpl implements BukuService{
+	@Autowired
+	private BukuDao bukuDao;
+
+	@Override
+	public void insert(Buku buku) throws Exception {
+		bukuDao.insert(buku);
+		
+	}
+
+	@Override
+	public void update(Buku buku) throws Exception {
+		bukuDao.update(buku);
+		
+	}
+
+	@Override
+	public void delete(Buku buku) throws Exception {
+		bukuDao.delete(buku);
+		
+	}
+
+	@Override
+	public Collection<Buku> listAll() throws Exception {
+		return bukuDao.listAll();
+	}
+
+}
