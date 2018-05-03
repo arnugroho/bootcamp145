486ebbaf70f0a3a28696cb2aadcfcf008cc010d3
Rezky Pratomo <rezkypratomo7@gmail.com>
2018-05-03 15:46:21 +0700

    Unit Code

--- src/main/java/com/xsis/bootcamp/util/GeneralVariable.java
+++ src/main/java/com/xsis/bootcamp/util/GeneralVariable.java
@@ -1,31 +1,26 @@
 package com.xsis.bootcamp.util;
 
 public interface GeneralVariable {
-	
+
 	public static final String RADIO_GROUP = "radiogroup";
 	public static final String SHOW_PROGRESS_BAR_TOP = "top";
 	public static final String ID_DEVICE = "01";
-	
-	
-	//User Role
+
+	// User Role
 	public static final Integer USER_ROLE_SUPERADMIN = 1;
 	public static final Integer USER_ROLE_ADMIN = 2;
 	public static final Integer USER_ROLE_USERSD = 3;
-	
-	
-	
+
 	public static final String ACTION_CREATE = "create";
 	public static final String ACTION_EDIT = "edit";
 	public static final String ACTION_VIEW = "view";
-	
-	// Format 
+
+	// Format
 	public static final String FORMAT_DATE = "yyyy-MM-dd";
 	public static final String FORMAT_DATETIME = "yyyy-MM-dd HH:mm:ss";
-	
-	
-	
+
 	// Operators
-	public class Operator{
+	public class Operator {
 		public static final String EQUALS = "=";
 		public static final String NOT_EQUALS = "<>";
 		public static final String GREATER_THEN = ">";
@@ -36,13 +31,14 @@ public interface GeneralVariable {
 		public static final String BETWEEN = "BETWEEN";
 		public static final String IS = "IS";
 	}
-	
-	//Status
+
+	// Status
 	public static final Integer ISDELETE_TRUE = 1;
 	public static final Integer ISDELETE_FALSE = 0;
-	
-	//KODE
+
+	// KODE
 	public static final String KODE_BUKU = "KB";
-	
+	public static final String KODE_ROLE = "RO";
+	public static final String KODE_UNIT = "UN";
 
 }
