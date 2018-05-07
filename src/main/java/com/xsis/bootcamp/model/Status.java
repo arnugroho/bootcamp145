1aa19685a4bef59498c2d85cda53464573685d56
Awang <haswandaimawan@gmail.com>
2018-05-07 16:57:29 +0700

    heehhe

--- src/main/java/com/xsis/bootcamp/model/Status.java
+++ src/main/java/com/xsis/bootcamp/model/Status.java
@@ -18,13 +18,6 @@ public class Status {
 	public void setId(int id) {
 		this.id = id;
 	}
-<<<<<<< HEAD
-
-	@Column(name = "status", length = 12)
-
-=======
-	@Column(name = "status", length = 12)
->>>>>>> d62b92fb700abb5c68f122708429e350b69aac88
 	public String getStatus() {
 		return status;
 	}
